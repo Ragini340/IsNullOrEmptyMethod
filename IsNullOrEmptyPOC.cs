7546c5c8d39d66562894a74d3ce0708de051761b
Ragini340 <ragini31bxr@gmail.com>
2022-11-29 16:37:43 +0530

    Add project files.

---
+++ IsNullOrEmptyPOC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNullOrEmptyMethod
+{
+   public class IsNullOrEmptyPOC
+    {
+        // To make similar method as IsNullOrEmpty
+        public static bool check(string s)
+        {
+            return (s == null || s == String.Empty) ? true : false;
+        }
+
+       public static void Main(string[] args)
+        {
+            string s1 = "RaginiRagini";
+
+            //Declaring String s2.Empty;
+            string s2 = "";
+            string s3 = null;
+
+            bool b1 = check(s1);
+            bool b2 = check(s2);
+            bool b3 = check(s3);
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+        }
+    }
+}
+
---
+++ StringMethodIsNullOrEmpty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNullOrEmptyMethod
+{
+    public class StringMethodIsNullOrEmpty
+    {
+        public static void Main(string[] args)
+        {
+            string s1 = "RaginiSingh";
+            
+            //Declaring String s2.Empty;
+            string s2 = "";
+
+            string s3 = null;
+
+            bool b1 = string.IsNullOrEmpty(s1);
+
+            // For String value Empty or "", return true
+            bool b2 = string.IsNullOrEmpty(s2);
+
+            // For String value null, return true
+            bool b3 = string.IsNullOrEmpty(s3);
+            
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+        }
+    }
+}
+
